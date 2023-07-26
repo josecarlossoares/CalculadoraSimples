3dac1f5604e4b37fd9c3711a74884fd4004bb4ea
José Carlos <69639102+josecarlossoares@users.noreply.github.com>
2023-07-26 03:22:17 +0000

    Resposta do projeto solicitado.

---
+++ script.js
@@ -0,0 +1,25 @@
+function calc(valor1, valor2, op){
+    let result;
+    if(op > 0 && op <5){
+        switch (op){
+            case 1:
+                result = valor1 + valor2
+                break;
+            case 2:
+                result = valor1 - valor2
+                breakk;
+            case 3:
+                result = valor1 * valor2
+                break;
+            case 4:
+                result = valor1/valor2
+                break;
+        }
+        return result;
+    }else{
+        return 0;
+    }
+}
+
+let resposta = calc(2, 2, 3)
+console.log(`O resultado foi ${resposta}`)
